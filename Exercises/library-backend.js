5387835d2a6f946373767f63b84d83329aaf6a12
Elvis Cops <elvis.cops93@gmail.com>
2025-09-29 22:19:14 +0200

    Exercise 8.7

--- Exercises/library-backend.js
+++ Exercises/library-backend.js
@@ -126,6 +126,10 @@ const typeDefs = `
             published: Int!
             genres: [String!]!
         ): Book
+        editAuthor(
+            name: String!
+            setBornTo: Int!
+        ): Author
     }
 `
 
@@ -160,6 +164,14 @@ const resolvers = {
             }
             books = books.concat(book)
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find(a => a.name === args.name)
+            if (!author) null
+
+            const updatedAuthor = {...author,born: args.setBornTo}
+            authors = authors.map(a => a.name === args.name ? updatedAuthor: a)
+            return updatedAuthor
         }
     }
 }
