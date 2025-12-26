b9cd15ff5b5cf447b2cfad95b2b073467f8d3e68
Elvis Cops <elvis.cops93@gmail.com>
2025-12-26 14:52:32 +0100

    Exercise 8.21 and 8.22 - already done

--- Exercises/library-frontend/src/components/Books.jsx
+++ Exercises/library-frontend/src/components/Books.jsx
@@ -3,11 +3,8 @@ import { useEffect, useState } from 'react'
 
 const Books = (props) => {
     const books = props.books
-    
     const genre = books ? Array.from(new Set(books.flatMap(b => b.genres || []))) : [];
-
     const [selectedGenre, setSelectedGenre] = useState(null);
-    
 
     useEffect(() => {
       setSelectedGenre(selectedGenre);
@@ -18,9 +15,7 @@ const Books = (props) => {
     }
 
     const handleGenreClick = (genre) => {
-        console.log('genre', genre);
       setSelectedGenre(genre);
-
     }   
 
   
@@ -32,47 +27,41 @@ const displayedBooks = books
 
 return (
         <div>
-                <h2>books</h2>
-
-                <table>
-                        <tbody>
-                                <tr>
-                                        <th>x</th>
-                                        <th>author</th>
-                                        <th>published</th>
-                                        <th>genre</th>
-                                </tr>
-                                {displayedBooks.map((a) => (
-                                        <tr key={a.title}>
-                                                <td>{a.title}</td>
-                                                <td>{a.author?.name ?? "Unknown"}</td>
-                                                <td>{a.published}</td>
-                                                <td>{a.genres?.join(", ")}</td>
-                                        </tr>
-                                ))}
-                        </tbody>
-                </table>
-                <div>
-                        <button
-                                onClick={() => handleGenreClick(null)}
-                                style={{
-                                        fontWeight: selectedGenre === null ? "600" : "normal",
-                                }}
-                        >
-                                all genres
-                        </button>
-                        {genre.map((g) => (
-                                <button
-                                        key={g}
-                                        onClick={() => handleGenreClick(g)}
-                                        style={{
-                                                fontWeight: selectedGenre === g ? "600" : "normal",
-                                        }}
-                                >
-                                        {g}
-                                </button>
-                        ))}
-                </div>
+            <h2>books</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>x</th>
+                        <th>author</th>
+                        <th>published</th>
+                        <th>genre</th>
+                    </tr>
+                    {displayedBooks.map((a) => (
+                        <tr key={a.title}>
+                            <td>{a.title}</td>
+                            <td>{a.author?.name ?? "Unknown"}</td>
+                            <td>{a.published}</td>
+                            <td>{a.genres?.join(", ")}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div>
+                <button onClick={() => handleGenreClick(null)}
+                        style={{ fontWeight: selectedGenre === null ? "600" : "normal"}}
+                >
+                all genres
+                </button>
+                {genre.map((g) => (
+                    <button
+                        key={g}
+                        onClick={() => handleGenreClick(g)}
+                        style={{ fontWeight: selectedGenre === g ? "600" : "normal"}}
+                    >
+                        {g}
+                    </button>
+                ))}
+            </div>
         </div>
 )
 }
